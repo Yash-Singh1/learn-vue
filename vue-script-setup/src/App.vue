02f47f55930dd4a36622bcfd9a231a8bbe067205
Yash-Singh1 <saiansh2525@gmail.com>
2022-06-26 14:09:53 -0500

    feat: vue script setup pattern

---
+++ vue-script-setup/src/App.vue
@@ -0,0 +1,54 @@
+<template>
+  <h1 @click="handleClick">My name is {{ name }}, I am {{ age }} years old.</h1>
+  <MyButton text="My button text" @buttonClicked="showAlert" />
+</template>
+
+<!-- <script>
+import { ref } from 'vue';
+import MyButton from '@/components/MyButton.vue';
+
+export default {
+  setup() {
+    const name = ref('Yash');
+    const age = ref(14);
+
+    function handleClick() {
+      name.value = 'Yosh';
+      age.value = 19;
+    }
+
+    function showAlert(age) {
+      alert(`Child button clicked with age ${age}`);
+    }
+
+    return {
+      name,
+      age,
+      handleClick,
+      showAlert,
+    };
+  },
+  components: {
+    MyButton,
+  },
+};
+</script> -->
+
+<script setup>
+// This will do the same in the original older script pattern but using the setup pattern
+
+import { ref } from 'vue';
+import MyButton from '@/components/MyButton.vue';
+
+const name = ref('Yash');
+const age = ref(14);
+
+function handleClick() {
+  name.value = 'Yosh';
+  age.value = 19;
+}
+
+function showAlert(age) {
+  alert(`Child button clicked with age ${age}`);
+}
+</script>
---
+++ vue-script-setup/src/components/MyButton.vue
@@ -0,0 +1,40 @@
+<template>
+  <!-- props.text also works -->
+  <!-- <button>{{ props.text }}</button> -->
+  <button @click="handleButtonClicked">{{ text }}</button>
+</template>
+
+<!-- <script>
+export default {
+  props: {
+    text: {
+      type: String,
+      default: 'no text specified',
+    },
+  },
+  emits: ['buttonClicked'],
+  setup(_ /* props */, context) {
+    function handleButtonClicked() {
+      context.emit('buttonClicked', Math.floor(Math.random() * 10 + 1));
+    }
+
+    return { handleButtonClicked };
+  },
+};
+</script> -->
+
+<script setup>
+// Does same logic as older script pattern but using newer setup pattern
+
+const props = defineProps({
+  text: {
+    type: String,
+    default: 'no text specified',
+  },
+});
+const emit = defineEmits(['buttonClicked']);
+
+function handleButtonClicked() {
+  emit('buttonClicked', Math.floor(Math.random() * 10 + 1));
+}
+</script>
