c35c7c3c520fd2cf03f819ef5da62dd2607aa7bd
Yash-Singh1 <saiansh2525@gmail.com>
2022-07-12 05:42:48 -0500

    feat: provide-and-inject

---
+++ vue-js-3-codevolution/provide-and-inject/src/App.vue
@@ -0,0 +1,36 @@
+<template>
+  <h2>App Username {{ username }}</h2>
+  <ComponentC />
+</template>
+
+<script>
+import ComponentC from './components/ComponentC.vue';
+
+export default {
+  name: 'App',
+  components: {
+    ComponentC,
+  },
+  data() {
+    return {
+      username: 'Vishwas',
+    };
+  },
+  provide() {
+    return {
+      username: this.username,
+    };
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
